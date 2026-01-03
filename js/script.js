4562993ac354375b9e7fb724b477f6147ca6c4ab
habibaahmed-01 <habibaahmed11358@gmail.com>
2026-01-03 14:32:47 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Smooth scroll
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
